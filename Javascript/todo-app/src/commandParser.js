90186ef8ef4d40399562f8117b51a225992af868
nikhil <nrpatil1314@gmail.com>
2025-11-27 21:31:34 +0530

    Javascript/todo-app : commandParser.js created

---
+++ Javascript/todo-app/src/commandParser.js
@@ -0,0 +1,55 @@
+// this file contains code which converts a string into an object of command and task title
+
+// function to convert inputed string into an object
+/*
+    function name - parseCommand
+    parameters - inputedString : string
+    returns - an object : contains command and task title
+*/
+function parseCommand(inputedString="add buy milk"){
+    // checking length of inputedString and validated type
+    if(typeof(inputedString)!='string'){
+        return null
+    }
+
+    // trimming extra spaces in inputedString
+    inputedString = inputedString.trim()
+
+    if(inputedString.length===0){
+        return null
+    }
+
+    // converting inputedString into an Array
+    /*
+        add buy milk
+
+        command : "add"
+        args : ["buy milk"]
+    */
+    // splits the inputedString having seperator " "
+    let stringWordArray = inputedString.split(" ")
+ 
+
+    // taking first word i.e. 0th index element as command
+    // removing 0th index element and store for command
+    let commandWord = stringWordArray.shift()
+    
+    console.log("in parse command method : "+commandWord+" - "+stringWordArray)
+
+    // create a string of remaning elements in stringWordArray
+    let argArray = []
+
+    if(stringWordArray.length!=0){
+        let argString = stringWordArray.join(" ")
+
+        // an array to pass as args argument in object
+        argArray = [argString]
+    }
+    
+   
+    // returning an object
+    return {command : commandWord , args : argArray}
+}
+
+
+console.log(parseCommand("complete 1"))
